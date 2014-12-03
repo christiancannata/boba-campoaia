48863a54edecfbf5accb0fdf6b094b596ee6e680
Christian Cannata <christiancannata@Christians-MacBook-Pro.local>
2014-12-04 00:27:19 +0100

    aggiunto slider come samsung grid

--- js/campoaia.js
+++ js/campoaia.js
@@ -1,23 +1,30 @@
 /**
  * Created by christiancannata on 03/12/14.
  */
-$(document).ready(function () {
+JQ(document).ready(function () {
 
-    $(".timeline").on('click','.month',function () {
+    JQ(".timeline").on('click', '.month', function () {
         var rel = $(this).attr('rel');
-        var clicked=$(this);
-        var current=$(".timeline .month-selected");
+        var clicked = $(this);
+        var current = $(".timeline .month-selected");
 
-        var month =current.find(".text").html();
+        var month = current.find(".text").html();
 
-        var nuovo_mese=$('<div class="month" rel="'+current.attr('rel')+'" number="'+current.attr('number')+'"><div class="image"></div> <div class="number"></div>'+current.attr('number')+'  <div class="triangle"> </div> </div>');
+        var nuovo_mese = JQ('<div class="month" rel="' + current.attr('rel') + '" number="' + current.attr('number') + '"><div class="image"></div> <div class="number"></div>' + current.attr('number') + '  <div class="triangle"> </div> </div>');
 
-        $( ".timeline .month-selected" ).replaceWith(nuovo_mese);
+        JQ(".timeline .month-selected").replaceWith(nuovo_mese);
 
 
-        var nuovo_selected=$('<div class="month-selected" rel="'+clicked.attr('rel')+'" number="'+clicked.attr('number')+'"><div class="text">'+clicked.attr('number')+'<br>'+clicked.attr('rel')+' </div> </div>');
+        var nuovo_selected = JQ('<div class="month-selected" rel="' + clicked.attr('rel') + '" number="' + clicked.attr('number') + '"><div class="text">' + clicked.attr('number') + '<br>' + clicked.attr('rel') + ' </div> </div>');
 
-        $( ".timeline .month[number="+clicked.attr('number')+"]" ).replaceWith(nuovo_selected);
+        $(".timeline .month[number=" + clicked.attr('number') + "]").replaceWith(nuovo_selected);
+
+    });
+
+
+    JQ(window).scroll(function () {
+        var corners = JQ('.corner');
+        ( JQ(window).scrollTop() > 0 ) ? corners.addClass('out') : corners.removeClass('out');
 
     });
 
